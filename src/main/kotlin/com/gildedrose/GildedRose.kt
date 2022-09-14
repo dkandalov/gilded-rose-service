849e53ea041ac16523109d270aae98e79a5842d3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-09-14 21:11:52 +0100

    initial commit

---
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -0,0 +1,56 @@
+package com.gildedrose
+
+import org.slf4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+data class Item(
+    val name: String,
+    var sellIn: Int,
+    var quality: Int
+) {
+    override fun toString() = "$name, $sellIn, $quality"
+}
+
+@Service
+class GildedRose {
+    @Autowired private val logger: Logger? = null
+
+    fun update(items: List<Item>) {
+        logger?.info("Updating items ${items.size}")
+
+        items.forEach { item ->
+            update(item)
+        }
+    }
+
+    fun update(item: Item) {
+        logger?.info("Updating item $item")
+
+        when (item.name) {
+            "Aged Brie"                                 -> item.updateQuality(1)
+            "Backstage passes to a TAFKAL80ETC concert" ->
+                if (item.sellIn <= 5) item.updateQuality(3)
+                else if (item.sellIn <= 10) item.updateQuality(2)
+                else item.updateQuality(1)
+            "Sulfuras, Hand of Ragnaros"                -> item.updateQuality(0)
+            else                                        -> item.updateQuality(-1)
+        }
+
+        item.sellIn -= if (item.name == "Sulfuras, Hand of Ragnaros") 0 else 1
+
+        if (item.sellIn < 0) {
+            when (item.name) {
+                "Aged Brie"                                 -> item.updateQuality(1)
+                "Backstage passes to a TAFKAL80ETC concert" -> item.updateQuality(-item.quality)
+                "Sulfuras, Hand of Ragnaros"                -> item.updateQuality(0)
+                else                                        -> item.updateQuality(-1)
+            }
+        }
+    }
+
+    private fun Item.updateQuality(change: Int) {
+        if (change == 0) return
+        quality = (quality + change).coerceIn(0, 50)
+    }
+}
---
+++ src/test/kotlin/com/gildedrose/WebControllerIntegrationTest.kt
@@ -0,0 +1,59 @@
+package com.gildedrose
+
+import kotlinx.datetime.LocalDate
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.given
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatus.UNAUTHORIZED
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class WebControllerIntegrationTest {
+    @Autowired
+    private lateinit var template: TestRestTemplate
+    @MockBean
+    private lateinit var itemsRepository: ItemsRepository
+
+    @Test
+    fun `get items for a date`() {
+        given(itemsRepository.loadItems(any())).willReturn(listOf(
+            Pair(LocalDate(2019, 1, 2), Item(name = "Box", sellIn = 1, quality = 2))
+        ))
+
+        val response = template.withBasicAuth("testUser", "secret")
+            .getForEntity("/items?date=2019-01-02", String::class.java)
+        assertThat(response.body).isEqualTo("""[{"name":"Box","sellIn":1,"quality":2}]""")
+    }
+
+    @Test
+    fun `get items for a date with no items`() {
+        given(itemsRepository.loadItems(any())).willReturn(emptyList())
+
+        val response = template.withBasicAuth("testUser", "secret")
+            .getForEntity("/items?date=2019-01-02", String::class.java)
+        assertThat(response.body).isEqualTo("""[]""")
+    }
+
+    @Test
+    fun `authentication is required to get items`() {
+        val response = template
+            .withBasicAuth("invalidUser", "pass")
+            .getForEntity("/items", String::class.java)
+        assertThat(response.statusCode).isEqualTo(UNAUTHORIZED)
+    }
+
+    @Test
+    fun `date is required to get items`() {
+        val response = template.withBasicAuth("testUser", "secret")
+            .getForEntity("/items", String::class.java)
+        assertThat(response.statusCode).isEqualTo(BAD_REQUEST)
+    }
+}
