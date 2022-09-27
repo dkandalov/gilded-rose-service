f169c26a65ed41c2a08afdabb79093cd3271ecb7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-09-27 08:48:32 +0100

    refactoring

--- src/test/kotlin/com/gildedrose/GildedRoseServiceTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseServiceTest.kt
@@ -6,16 +6,11 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.given
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.mockito.kotlin.mock
 
-@SpringBootTest
 class GildedRoseServiceTest {
-    @Autowired
-    private lateinit var service: GildedRoseService
-    @MockBean
-    private lateinit var itemsRepository: ItemsRepository
+    private val itemsRepository = mock<ItemsRepository>()
+    private val service = GildedRoseServiceImpl(itemsRepository)
 
     @BeforeEach
     fun setup() {
@@ -28,12 +23,18 @@ class GildedRoseServiceTest {
     @Test
     fun `items which were added on the same date`() {
         val items = service.items(LocalDate.parse("2019-01-02"))
-        assertEquals(listOf(Item("Box", 10, 20), Item("Aged Brie", 20, 30)), items)
+        assertEquals(listOf(
+            Item("Box", 10, 20),
+            Item("Aged Brie", 20, 30)
+        ), items)
     }
 
     @Test
     fun `items which were added day before`() {
         val items = service.items(LocalDate.parse("2019-01-04"))
-        assertEquals(listOf(Item("Box", 8, 18), Item("Aged Brie", 18, 32)), items)
+        assertEquals(listOf(
+            Item("Box", 8, 18),
+            Item("Aged Brie", 18, 32)
+        ), items)
     }
 }
