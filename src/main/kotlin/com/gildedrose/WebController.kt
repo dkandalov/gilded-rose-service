7a440ff68686a72b007c65b98b6facb293aaeb81
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-09-29 23:26:20 +0100

    updated WebController

--- src/main/kotlin/com/gildedrose/WebController.kt
+++ src/main/kotlin/com/gildedrose/WebController.kt
@@ -1,14 +1,22 @@
 package com.gildedrose
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import kotlinx.datetime.toLocalDate
+import kotlinx.datetime.LocalDate
+import org.http4k.core.Body
+import org.http4k.core.Filter
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
+import org.http4k.core.NoOp
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.Status.Companion.OK
+import org.http4k.core.then
+import org.http4k.core.with
 import org.http4k.filter.ServerFilters.BasicAuth
+import org.http4k.filter.ServerFilters.CatchLensFailure
+import org.http4k.format.Jackson.auto
+import org.http4k.lens.BiDiMapping
+import org.http4k.lens.Query
+import org.http4k.lens.map
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.slf4j.Logger
@@ -19,19 +27,27 @@ class WebController(
     newLogger: (String) -> Logger = ::defaultLogger
 ) : HttpHandler {
     private val logger = newLogger(javaClass.simpleName)
-    private val objectMapper = jacksonObjectMapper()
 
-    private val routes = routes(
+    private val basicAuth = BasicAuth(realm = "") { credentials ->
+        credentials.user in config.users && credentials.password == "secret"
+    }
+    private val dateLens = Query.map(kotlinxLocalDate).required("date")
+    private val bodyLens = Body.auto<List<Item>>().toLens()
+
+    private val filters = Filter.NoOp.then(basicAuth).then(CatchLensFailure())
+    private val routes = filters.then(routes(
         "items" bind GET to { request ->
-            val items = request.query("date")?.let {
-                logger.info("Requested items for $it")
-                gildedRoseService.items(it.toLocalDate())
-            }
-            if (items == null) Response(BAD_REQUEST).body("date parameter is required")
-            else Response(OK).header("Content-Type", "application/json")
-                .body(objectMapper.writeValueAsString(items))
+            val date: LocalDate = dateLens(request)
+            logger.info("Requested items for $date")
+            val items = gildedRoseService.items(date)
+            Response(OK).with(bodyLens of items)
         }
-    ).withFilter(BasicAuth("") { it.user in config.users && it.password == "secret" })
+    ))
 
     override fun invoke(request: Request) = routes(request)
 }
+
+private val kotlinxLocalDate = BiDiMapping<String, LocalDate>(
+    asOut = { LocalDate.parse(it) },
+    asIn = { it.toString() }
+)
