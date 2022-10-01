28c6b9d3f74eb2ef039303aadc28a55081628714
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-10-01 20:20:15 +0100

    refactoring

--- src/main/kotlin/com/gildedrose/GildedRoseService.kt
+++ src/main/kotlin/com/gildedrose/GildedRoseService.kt
@@ -5,16 +5,16 @@ import com.gildedrose.domain.Item
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.minus
 import org.slf4j.Logger
-import org.springframework.beans.factory.annotation.Autowired
 
 class GildedRoseService(
     private val repository: ItemsRepository,
-    private val gildedRose: GildedRose = GildedRose()
+    private val gildedRose: GildedRose = GildedRose(),
+    newLogger: (String) -> Logger = ::defaultLogger
 ) {
-    @Autowired private val logger: Logger? = null
+    private val logger = newLogger(javaClass.simpleName)
 
     fun items(asOfDate: LocalDate): List<Item> {
-        logger?.info("Loading items for $asOfDate")
+        logger.info("Loading items for $asOfDate")
 
         val items = repository.loadItems(createdOnOrBefore = asOfDate)
         return items.map { (createdDate, item) ->
