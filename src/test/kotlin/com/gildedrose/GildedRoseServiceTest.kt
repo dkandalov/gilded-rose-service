519b046bcc7df97a7b45dd5106d2e18752a7f11a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-10-01 20:18:03 +0100

    refactoring

--- src/test/kotlin/com/gildedrose/GildedRoseServiceTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseServiceTest.kt
@@ -11,7 +11,7 @@ import org.mockito.kotlin.mock
 
 class GildedRoseServiceTest {
     private val itemsRepository = mock<ItemsRepository>()
-    private val service = GildedRoseServiceImpl(itemsRepository)
+    private val service = GildedRoseService(itemsRepository)
 
     @BeforeEach
     fun setup() {
