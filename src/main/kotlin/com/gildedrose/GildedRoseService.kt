1c6e8d86654f7db1fb01486cb887f037d8fe83b0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-10-01 13:58:41 +0100

    refactoring

--- src/main/kotlin/com/gildedrose/GildedRoseService.kt
+++ src/main/kotlin/com/gildedrose/GildedRoseService.kt
@@ -8,18 +8,14 @@ import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
-interface GildedRoseService {
-    fun items(asOfDate: LocalDate): List<Item>
-}
-
 @Service
-class GildedRoseServiceImpl(
+class GildedRoseService(
     @Autowired val repository: ItemsRepository,
     val gildedRose: GildedRose = GildedRose()
-) : GildedRoseService {
+) {
     @Autowired private val logger: Logger? = null
 
-    override fun items(asOfDate: LocalDate): List<Item> {
+    fun items(asOfDate: LocalDate): List<Item> {
         logger?.info("Loading items for $asOfDate")
 
         val items = repository.loadItems(createdOnOrBefore = asOfDate)
