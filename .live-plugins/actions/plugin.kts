aa369911a97e21e9d23b4c128a985b67e2b1b1e6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-10-03 22:31:18 +0100

    improved WebController

--- .live-plugins/actions/plugin.kts
+++ .live-plugins/actions/plugin.kts
@@ -164,27 +164,29 @@ import org.slf4j.Logger
 
 class WebController(
     private val config: Config,
-    private val gildedRoseService: GildedRoseService
+    gildedRoseService: GildedRoseService
 ) : HttpHandler {
     private val logger = newLogger(javaClass.simpleName)
-
-    private val basicAuth = BasicAuth(realm = "") { credentials ->
+    private val authFilter = BasicAuth(realm = "") { credentials ->
         credentials.user in config.users && credentials.password == "secret"
     }
-    private val dateLens = Query.map(kotlinxLocalDate).required("date")
-    private val bodyLens = Body.auto<List<Item>>().toLens()
-
-    private val filters = Filter.NoOp.then(basicAuth).then(CatchLensFailure())
-    private val routes = filters.then(routes(
-        "items" bind GET to { request ->
-            val date: LocalDate = dateLens(request)
-            logger.info("Requested items for ${'$'}date")
-            val items = gildedRoseService.items(date)
-            Response(OK).with(bodyLens of items)
-        }
-    ))
+    private val routes = authFilter.then(CatchLensFailure())
+        .then(routes(
+            "items" bind GET to listItems(gildedRoseService, logger)
+        ))
 
     override fun invoke(request: Request) = routes(request)
+}
+
+private fun listItems(gildedRoseService: GildedRoseService, logger: Logger): HttpHandler {
+    val dateLens = Query.map(kotlinxLocalDate).required("date")
+    val bodyLens = Body.auto<List<Item>>().toLens()
+    return { request ->
+        val date = dateLens(request)
+        logger.info("Requested items for ${'$'}date")
+        val items = gildedRoseService.items(date)
+        Response(OK).with(bodyLens of items)
+    }
 }
 
 private val kotlinxLocalDate = BiDiMapping<String, LocalDate>(
