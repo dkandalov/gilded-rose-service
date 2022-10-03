76ebda3faa52f8fc84595dd8a6dbdef9dd3ed587
Duncan McGregor <duncan@oneeyedmen.com>
2022-10-03 11:20:09 +0100

    Better GildedRose

--- src/main/kotlin/com/gildedrose/domain/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/domain/GildedRose.kt
@@ -3,7 +3,7 @@ package com.gildedrose.domain
 import org.springframework.stereotype.Service
 
 @Service
-class GildedRose {
+class GildedRose() {
     fun update(items: List<Item>) {
         items.forEach { item ->
             update(item)
@@ -11,30 +11,76 @@ class GildedRose {
     }
 
     fun update(item: Item) {
-        when (item.name) {
-            "Aged Brie"                                 -> item.updateQuality(1)
-            "Backstage passes to a TAFKAL80ETC concert" ->
-                if (item.sellIn <= 5) item.updateQuality(3)
-                else if (item.sellIn <= 10) item.updateQuality(2)
-                else item.updateQuality(1)
-            "Sulfuras, Hand of Ragnaros"                -> item.updateQuality(0)
-            else                                        -> item.updateQuality(-1)
-        }
-
-        item.sellIn -= if (item.name == "Sulfuras, Hand of Ragnaros") 0 else 1
-
-        if (item.sellIn < 0) {
-            when (item.name) {
-                "Aged Brie"                                 -> item.updateQuality(1)
-                "Backstage passes to a TAFKAL80ETC concert" -> item.updateQuality(-item.quality)
-                "Sulfuras, Hand of Ragnaros"                -> item.updateQuality(0)
-                else                                        -> item.updateQuality(-1)
-            }
-        }
-    }
-
-    private fun Item.updateQuality(change: Int) {
-        if (change == 0) return
-        quality = (quality + change).coerceIn(0, 50)
+        item.type.update(item)
     }
 }
+
+val Item.type
+    get() = when (name) {
+        "Backstage passes to a TAFKAL80ETC concert" -> PASS
+        "Aged Brie" -> BRIE
+        "Sulfuras, Hand of Ragnaros" -> SULFURAS
+        else -> NORMAL
+    }
+
+open class ItemType {
+
+    fun update(item: Item) {
+        age(item)
+        degrade(item)
+    }
+
+    protected open fun age(item: Item) {
+        item.sellIn -= 1
+    }
+
+    protected open fun degrade(item: Item) {
+        item.setQuality(
+            degradation(item)
+        )
+    }
+
+    protected open fun degradation(item: Item) = when {
+        item.sellIn < 0 -> item.quality - 2
+        else -> item.quality - 1
+    }
+
+    protected fun Item.setQuality(quality: Int) {
+        this.quality = quality.coerceIn(0, 50)
+    }
+}
+
+object PASS : ItemType() {
+    override fun degrade(item: Item) {
+        item.setQuality(
+            degradation(item)
+        )
+    }
+
+    override fun degradation(item: Item) = when {
+        item.sellIn < 0 -> 0
+        item.sellIn < 5 -> item.quality + 3
+        item.sellIn < 10 -> item.quality + 2
+        else -> item.quality + 1
+    }
+}
+
+object BRIE : ItemType() {
+    override fun degrade(item: Item) {
+        item.setQuality(
+            degradation(item)
+        )
+    }
+
+    override fun degradation(item: Item) = if (item.sellIn < 0) {
+        item.quality + 2
+    } else
+        item.quality + 1
+}
+
+object SULFURAS : ItemType() {
+    override fun age(item: Item) {}
+    override fun degrade(item: Item) {}
+}
+
+object NORMAL : ItemType()
